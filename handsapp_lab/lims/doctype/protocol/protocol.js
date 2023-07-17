6c0b8475e8ccdc7fab192538bbd3b2932c12cc34
gchartas <gchartasol@gmail.com>
2023-07-17 15:39:38 +0000

    Protocol Creation

---
+++ handsapp_lab/lims/doctype/protocol/protocol.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, George Chartas and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Protocol', {
+	// refresh: function(frm) {
+
+	// }
+});
