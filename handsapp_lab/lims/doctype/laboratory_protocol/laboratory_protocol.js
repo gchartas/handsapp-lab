e7049a5b6023e8f86213435cc9ccd027f96b5d2a
gchartas <gchartasol@gmail.com>
2023-08-03 17:19:34 +0000

    Insert Laboratory Protocol

--- handsapp_lab/lims/doctype/laboratory_protocol/laboratory_protocol.js
+++ handsapp_lab/lims/doctype/laboratory_protocol/laboratory_protocol.js
@@ -2,7 +2,14 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Laboratory Protocol', {
-	// refresh: function(frm) {
+    set_dates: function(frm) {
+        // Set the publish_date to today
+        frm.set_value('publish_date', frappe.datetime.now_date());
+    },
+    publish_date: function(frm){
+        var p_date = new Date(frm.doc.publish_date);
+        var tyfpd = new Date(p_date.setFullYear(p_date.getFullYear() + 3));
+        frm.set_value('review_date', tyfpd);
+    }
 
-	// }
 });
--- handsapp_lab/lims/doctype/laboratory_protocol/laboratory_protocol.py
+++ handsapp_lab/lims/doctype/laboratory_protocol/laboratory_protocol.py
@@ -1,8 +1,11 @@
 # Copyright (c) 2023, George Chartas and contributors
 # For license information, please see license.txt
 
-# import frappe
 from frappe.model.document import Document
+from frappe.model.naming import make_autoname
+import frappe
 
 class LaboratoryProtocol(Document):
-	pass
+    def before_insert(self):
+        # frappe.msgprint('Poutanakia FTW')   Debug message
+        self.code_doc = make_autoname('LP-.####')
